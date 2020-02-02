7f43283677e243397ab34d7d6629884093053dc2
za-zhangxiaobo <zhangxiaobo@zhongan.io>
2020-02-02 18:12:57 +0800

    新增快速排序

--- src/algorithm/sort/index.js
+++ src/algorithm/sort/index.js
@@ -100,11 +100,45 @@ const mergeArr = (left, right) => {
 }
 
 
+//快速排序
+const swap = (arr, i, j) => {
+    const temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+}
+
+// 获取 pivot 交换完后的index
+const partition = (arr, pivot, left, right) => {
+    const pivotVal = arr[pivot]
+    let startIndex = left
+    for (let i = left; i < right; i++) {
+        if (arr[i] < pivotVal) {
+            swap(arr, i, startIndex)
+            startIndex++
+        }
+    }
+    swap(arr, startIndex, pivot)
+    return startIndex
+}
+
+const quickSort = (arr, left, right) => {
+    if (left < right) {
+        let pivot = right
+        let partitionIndex = partition(arr, pivot, left, right)
+        quickSort(arr, left, partitionIndex - 1 < left ? left : partitionIndex - 1)
+        quickSort(arr, partitionIndex + 1 > right ? right : partitionIndex + 1, right)
+    }
+    return arr;
+}
+
+
+
 let testList = [3,7,2,5,9,1,1];
 
 // bubbleSort(testList);
 
 // insertSort(testList);
 // selectSort(testList);
-console.log(mergeSort(testList));
+// console.log(mergeSort(testList));
+console.log(quickSort(testList, 0 , testList.length-1));
 // console.log(testList);
