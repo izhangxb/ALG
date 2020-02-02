f34baf4ca3ff754bb6c48982a03b7d0bbe1c24fc
za-zhangxiaobo <zhangxiaobo@zhongan.io>
2020-02-02 12:53:39 +0800

    新增归并排序

--- src/algorithm/sort/index.js
+++ src/algorithm/sort/index.js
@@ -68,11 +68,43 @@ let selectSort = (list)=>{
     }
 };
 
+
+//归并排序
+const mergeSort = (arr) => {
+    // 当任意数组分解到只有一个时返回。
+    if (arr.length <= 1) return arr
+    const middle = Math.floor(arr.length / 2) // 找到中间值
+    const left = arr.slice(0, middle) // 分割数组
+    const right = arr.slice(middle)
+    // 递归 分解 合并
+    return mergeArr(mergeSort(left), mergeSort(right))
+}
+
+
+const mergeArr = (left, right) => {
+    let temp = []
+    let leftIndex = 0
+    let rightIndex = 0
+    // 判断2个数组中元素大小，依次插入数组
+    while (left.length > leftIndex && right.length > rightIndex) {
+        if (left[leftIndex] <= right[rightIndex]) {
+            temp.push(left[leftIndex])
+            leftIndex++
+        } else {
+            temp.push(right[rightIndex])
+            rightIndex++
+        }
+    }
+    // 合并 多余数组
+    return temp.concat(left.slice(leftIndex)).concat(right.slice(rightIndex))
+}
+
+
 let testList = [3,7,2,5,9,1,1];
 
 // bubbleSort(testList);
 
 // insertSort(testList);
-selectSort(testList);
-
-console.log(testList);
+// selectSort(testList);
+console.log(mergeSort(testList));
+// console.log(testList);
